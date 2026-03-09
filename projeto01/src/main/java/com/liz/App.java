1d78b1f6d834fdf2172b9cb4ee95c55c4f4f6ba1
Eliza Müller <75331265+LizzieMuller@users.noreply.github.com>
2026-03-09 13:53:53 +0000

    Primeiro projeto teste

---
+++ projeto01/src/main/java/com/liz/App.java
@@ -0,0 +1,8 @@
+package com.liz;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, world! Projeto projeto01 criado com sucesso.");
+        
+    }
+}
